e59f6bd9af96b9fe4ee2be3350044dcbc45ad40b
Ryzii74 <Ryzii@yandex.ru>
2017-02-28 23:17:05 +0300

    + add basic game logic

--- client/javascript/components/gameField.js
+++ client/javascript/components/gameField.js
@@ -2,39 +2,373 @@ var gameField = new Vue({
     el: '#gameField',
     data: {
         field: [
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0]
+            [null, null, null, null],
+            [null, null, null, null],
+            [null, null, null, null],
+            [null, null, null, null]
         ],
         moves: [
-            [1, 2, 3, 4],
-            [5, 6, 7, 8],
-            [9, 10, 11, 12],
-            [13, 14, 15, 16]
+            [
+                {
+                    isRed: true,
+                    isCircle: true,
+                    isBig: true,
+                    isEmpty: true
+                },
+                {
+                    isRed: true,
+                    isCircle: true,
+                    isBig: true,
+                    isEmpty: false
+                },
+                {
+                    isRed: true,
+                    isCircle: true,
+                    isBig: false,
+                    isEmpty: true
+                },
+                {
+                    isRed: true,
+                    isCircle: false,
+                    isBig: true,
+                    isEmpty: true
+                }
+            ],
+            [
+                {
+                    isRed: false,
+                    isCircle: true,
+                    isBig: true,
+                    isEmpty: true
+                },
+                {
+                    isRed: false,
+                    isCircle: false,
+                    isBig: true,
+                    isEmpty: true
+                },
+                {
+                    isRed: false,
+                    isCircle: true,
+                    isBig: false,
+                    isEmpty: true
+                },
+                {
+                    isRed: false,
+                    isCircle: true,
+                    isBig: true,
+                    isEmpty: false
+                }
+            ],
+            [
+                {
+                    isRed: true,
+                    isCircle: false,
+                    isBig: false,
+                    isEmpty: true
+                },
+                {
+                    isRed: true,
+                    isCircle: true,
+                    isBig: false,
+                    isEmpty: false
+                },
+                {
+                    isRed: true,
+                    isCircle: false,
+                    isBig: true,
+                    isEmpty: false
+                },
+                {
+                    isRed: false,
+                    isCircle: false,
+                    isBig: false,
+                    isEmpty: false
+                }
+            ],
+            [
+                {
+                    isRed: true,
+                    isCircle: false,
+                    isBig: false,
+                    isEmpty: false
+                },
+                {
+                    isRed: false,
+                    isCircle: true,
+                    isBig: false,
+                    isEmpty: false
+                },
+                {
+                    isRed: false,
+                    isCircle: false,
+                    isBig: true,
+                    isEmpty: false
+                },
+                {
+                    isRed: false,
+                    isCircle: false,
+                    isBig: false,
+                    isEmpty: true
+                }
+            ]
         ],
         yourTurn: true,
-        selectedMove: null
+        selectedMove: null,
+        isGameEnded: false,
+        isGameWined: false
     },
     methods: {
         selectMove: function (rowIndex, itemIndex) {
-            if (!this.yourTurn) return;
+            //if (!this.yourTurn) return;
+            if (this.isGameWined || this.isGameEnded) return;
+            if (this.moves[rowIndex][itemIndex] === null) return;
 
-            
             this.selectedMove = {};
             this.selectedMove.x = rowIndex;
             this.selectedMove.y = itemIndex;
-
         },
         makeMove: function (rowIndex, itemIndex) {
             if (!this.selectedMove) return;
             this.field[rowIndex][itemIndex] = this.moves[this.selectedMove.x][this.selectedMove.y];
+            this.moves[this.selectedMove.x][this.selectedMove.y] = null;
             this.selectedMove = null;
 
-            this.checkIsWin();
+            this.checkIsGameWined();
+            this.checkIsGameEnded();
+
+            this.yourTurn = !this.yourTurn;
         },
-        checkIsWin: function () {
-            console.log('no win')
+        isItemSelected: function (rowIndex, itemIndex) {
+            return this.selectedMove
+                && this.selectedMove.x === rowIndex
+                && this.selectedMove.y === itemIndex;
+        },
+        getTurnHeader: function () {
+            if (this.isGameEnded) return "Игра окончена!";
+            if (this.isGameWined) {
+                return (this.yourTurn) ? "Победил игрок 2!" : "Победил игрок 1";
+            }
+            return this.yourTurn ? "Ход игрока 1" : "Ход игрока 2";
+        },
+        checkIsGameWined: function () {
+            let isGameWined = false;
+
+            LINES.forEach(line => {
+                var lineItems = this.getLineItems(line);
+                if (lineItems.filter(el => !el).length) return;
+
+                PROPS.forEach(prop => {
+                    var itemWithPropCount = lineItems.filter(el => el[prop]).length;
+                    if (itemWithPropCount % PROPS.length === 0) isGameWined = true;
+                });
+            });
+
+            this.isGameWined = isGameWined;
+        },
+        getLineItems: function(line) {
+            let items = [];
+            line.forEach(el => {
+                items.push(this.field[el.x][el.y]);
+            });
+
+            return items;
+        },
+        checkIsGameEnded: function () {
+            let isGameEnded = true;
+            let max = this.field.length;
+            for (let i = 0; i < max; i++) {
+                for (let j = 0; j < max; j++) {
+                    if (this.field[i][j] === null) isGameEnded = false;
+                }
+            }
+
+            this.isGameEnded = isGameEnded;
         }
     }
 });
+
+const PROPS = [ 'isRed', 'isBig', 'isEmpty', 'isCircle' ];
+
+const LINES = [
+    [
+        {
+            x: 0,
+            y: 0
+        },
+        {
+            x: 0,
+            y: 1
+        },
+        {
+            x: 0,
+            y: 2
+        },
+        {
+            x: 0,
+            y: 3
+        }
+    ],
+    [
+        {
+            x: 1,
+            y: 0
+        },
+        {
+            x: 1,
+            y: 1
+        },
+        {
+            x: 1,
+            y: 2
+        },
+        {
+            x: 1,
+            y: 3
+        }
+    ],
+    [
+        {
+            x: 2,
+            y: 0
+        },
+        {
+            x: 2,
+            y: 1
+        },
+        {
+            x: 2,
+            y: 2
+        },
+        {
+            x: 2,
+            y: 3
+        }
+    ],
+    [
+        {
+            x: 3,
+            y: 0
+        },
+        {
+            x: 3,
+            y: 1
+        },
+        {
+            x: 3,
+            y: 2
+        },
+        {
+            x: 3,
+            y: 3
+        }
+    ],
+    [
+        {
+            x: 0,
+            y: 0
+        },
+        {
+            x: 1,
+            y: 0
+        },
+        {
+            x: 2,
+            y: 0
+        },
+        {
+            x: 3,
+            y: 0
+        }
+    ],
+    [
+        {
+            x: 0,
+            y: 1
+        },
+        {
+            x: 1,
+            y: 1
+        },
+        {
+            x: 2,
+            y: 1
+        },
+        {
+            x: 3,
+            y: 1
+        }
+    ],
+    [
+        {
+            x: 0,
+            y: 2
+        },
+        {
+            x: 1,
+            y: 2
+        },
+        {
+            x: 2,
+            y: 2
+        },
+        {
+            x: 3,
+            y: 2
+        }
+    ],
+    [
+        {
+            x: 0,
+            y: 3
+        },
+        {
+            x: 1,
+            y: 3
+        },
+        {
+            x: 2,
+            y: 3
+        },
+        {
+            x: 3,
+            y: 3
+        }
+    ],
+    [
+        {
+            x: 0,
+            y: 0
+        },
+        {
+            x: 1,
+            y: 1
+        },
+        {
+            x: 2,
+            y: 2
+        },
+        {
+            x: 3,
+            y: 3
+        }
+    ],
+    [
+        {
+            x: 0,
+            y: 3
+        },
+        {
+            x: 1,
+            y: 2
+        },
+        {
+            x: 2,
+            y: 1
+        },
+        {
+            x: 3,
+            y: 0
+        }
+    ]
+];
