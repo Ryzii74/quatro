56ebe63a32de61398d3bbc360c2550031dc5f6e2
Ryzii74 <Ryzii@yandex.ru>
2017-03-01 20:19:50 +0300

    - remove unused lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,26 +12,18 @@ module.exports = {
     ],
     'rules': {
         "global-require": 1,
+        "import/no-dynamic-require": 1,
+        
         "no-console": 0,
         "no-plusplus": 0,
         "arrow-parens": 0,
         "indent": ["error", 4],
         "comma-dangle": ["error", "never"],
         "import/newline-after-import": 0,
-        "import/no-dynamic-require": 1,
         "eol-last": ["error", "never"],
 
         // только на клиенте пока нет нормального импорта
         "no-unused-vars": 0,
-        "no-undef": 0,
-
-        'import/extensions': ['error', 'always', {
-            'js': 'never',
-            'vue': 'never'
-        }],
-        'import/no-extraneous-dependencies': ['error', {
-            'optionalDependencies': ['test/unit/index.js']
-        }],
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+        "no-undef": 0
     }
 };
