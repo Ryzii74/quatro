c36db426934a01ed98561dcb80453bfdc3f329e4
Ryzii74 <Ryzii@yandex.ru>
2017-03-04 23:46:25 +0300

    ~ init vue in app

--- client/javascript/app.js
+++ client/javascript/app.js
@@ -2,7 +2,13 @@ Vue.use(Vuex);
 
 const Connection = require('./libs/connection');
 const Router = require('./libs/router');
+const store = require('./stores/index.js');
 
 Connection.init(() => {
-    Router.init();
+    const router = Router.init();
+
+    new Vue({
+        store,
+        router
+    }).$mount('#app');
 });
--- client/javascript/libs/router.js
+++ client/javascript/libs/router.js
@@ -1,6 +1,5 @@
 const MainPage = require('../components/mainPage.vue');
 const GameField = require('../components/gameField/index.vue');
-const store = require('../stores/index.js');
 
 module.exports = {
     init() {
@@ -9,14 +8,9 @@ module.exports = {
             { path: '/game', component: GameField }
         ];
 
-        const router = new VueRouter({
-            //mode: 'history',
+        return new VueRouter({
+            // mode: 'history',
             routes // сокращение от routes: routes
         });
-
-        new Vue({
-            store,
-            router
-        }).$mount('#app');
     }
 };
