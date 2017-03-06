da077c3c2441abe8ad2c50de2e9a1c3ef9d2c7e3
Ryzii74 <Ryzii@yandex.ru>
2017-03-06 22:39:01 +0300

    + add sign up logic

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,20 +1,13 @@
+const async = require('async');
 const Db = require('../libs/db');
 
 function getHash(data) {
     return JSON.stringify(data);
 }
 
-module.exports = socket => {
-    socket.on('auth', ({ login }, callback) => {
-        socket.login = login;
-        callback({
-            success: true,
-            data: {
-                login
-            }
-        });
-    });
+const INTERNAL_ERROR = 'внутренняя ошибка';
 
+module.exports = socket => {
     socket.on('login', (data, callback) => {
         Db.get().collection('users').findOne({
             hash: data.hash || getHash(data)
@@ -24,18 +17,18 @@ module.exports = socket => {
                 console.error(data);
                 return callback({
                     success: false,
-                    error: "Внутренняя ошибка"
+                    error: INTERNAL_ERROR
                 });
             }
 
             if (!user) {
                 return callback({
                     success: false,
-                    error: "Пользователь не найден"
+                    error: 'Пользователь не найден'
                 });
             }
 
-            socket.id = user._id.toString();
+            socket.userId = user._id.toString();
             callback({
                 success: true,
                 data: {
@@ -56,6 +49,49 @@ module.exports = socket => {
     });
 
     socket.on('signup', (data, callback) => {
-        
+        const hash = getHash(data);
+
+        async.waterfall([
+            callback => Db.get().collection('users').findOne({
+                login: data.login
+            }, (err, user) => {
+                if (err) {
+                    console.error('error getting user with login', err);
+                    console.error(data);
+                    return callback(INTERNAL_ERROR);
+                }
+                if (user) return callback('пользователь с таким именем уже зарегистрирован');
+
+                callback();
+            }),
+            callback => Db.get().collection('users').insertOne({
+                login: data.login,
+                password: data.password,
+                hash
+            }, (err, result) => {
+                if (err) {
+                    console.error('error getting user with login', err);
+                    console.error(data);
+                    return callback(INTERNAL_ERROR);
+                }
+
+                callback(null, result.insertedId);
+            })
+        ], (err, userId) => {
+            if (err) return callback({
+                success: false,
+                error: err
+            });
+
+            socket.userId = userId; 
+            callback({
+                success: true,
+                data: {
+                    login: data.login,
+                    hash
+                }
+            })
+        });
+
     });
 };
