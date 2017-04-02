50857bd4925eece843b59ac3cf47859fb4e8de35
Ryzii74 <Ryzii@yandex.ru>
2017-04-02 20:08:39 +0300

    + move game field css

--- client/javascript/components/gameField/field.vue
+++ client/javascript/components/gameField/field.vue
@@ -39,4 +39,54 @@
 <style lang="stylus" scoped>
 #field .win
     background-color #00f;
+
+#field table td
+    border 1px solid #000;
+    padding 10px;
+
+
+#field #moves .selected
+    background-color #600013;
+
+
+#field .item
+    width 25px;
+    height 25px;
+    position relative;
+    margin-left 12px;
+    margin-bottom 12px;
+
+#field .item.big
+    width 50px;
+    height 50px;
+    margin-left 0;
+    margin-bottom 0;
+
+#field .item.green
+    background-color #0f0;
+
+#field .item.red
+    background-color #f00;
+
+#field .item.circle,
+#field .circle .item-inner.empty
+    border-radius 50%;
+
+#field .item-inner.empty
+    position relative;
+    background-color #fff;
+    width 15px;
+    height 15px;
+    left 5px;
+    top 5px;
+
+#field .big .item-inner.empty
+    width 40px;
+    height 40px;
+
+#field .item.empty
+    width 38px;
+    height 38px;
+    background-color #fff;
+
 </style>
